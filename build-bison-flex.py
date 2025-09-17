ac8e8c13364b1d0a867c3fec98088f9345bf3f03
carlHR <carloshritzmann@gmail.com>
2025-09-17 11:43:39 -0300

    Update

---
+++ build-bison-flex.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Setting App-Specific flags:
+#  - bison flags:  --bison-<flag> <N> <N-params>
+#  - lex flags:   --lex-<flags> <N> <N-params>
+
+# Build-Bison-Lex flags:
+#  --bison-src <src>
+#  --lex-src <src>
+
+def main():
+	pass
